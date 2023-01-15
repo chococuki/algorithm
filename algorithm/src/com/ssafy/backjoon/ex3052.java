3f200c4f815227055a08b32f5f0365f4a535f375
Hyunduk <Hyunduk@DESKTOP-HANV34T>
2023-01-15 19:27:54 +0900

    2023.01.15

--- algorithm/src/com/ssafy/backjoon/ex3052.java
+++ algorithm/src/com/ssafy/backjoon/ex3052.java
@@ -1,5 +1,36 @@
 package com.ssafy.backjoon;
 
-public class ex3052 {
+import java.io.*;
+import java.util.*;
 
+public class ex3052 {
+	static int[] m = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+	static boolean check(int[] m, int j) {
+		for(int i=0; i<j; i++) {
+			if(m[i] == m[j] && i != j) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] m = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+		int count = 0;
+
+		for(int i=0; i<10; i++) {
+			m[i] = Integer.parseInt(br.readLine())%42;
+		}
+		
+		for(int i=0; i<10; i++) {
+			if(check(m, i)) count++;
+		}
+		
+		System.out.println(count);
+		
+	}
 }
