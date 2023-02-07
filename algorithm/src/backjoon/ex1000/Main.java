b3bc239f1bec1bd658c00cd868b8d7f8fd3b5dc2
chococuki <kkang13277@naver.com>
2023-02-07 22:42:43 +0900

    move dir

--- algorithm/src/backjoon/ex1000/Main.java
+++ algorithm/src/backjoon/ex1000/Main.java
@@ -1,13 +1,13 @@
-package backjoon.tmp;
+package backjoon.ex1000;
 
 import java.io.*;
 
-public class ex1001 {
+public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String[] s = br.readLine().split(" ");
 		
-		System.out.println(Integer.parseInt(s[0]) - Integer.parseInt(s[1]));
+		System.out.println(Integer.parseInt(s[0]) + Integer.parseInt(s[1]));
 	}
 }
