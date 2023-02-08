d254f37d14494576c9fdeab8700820b986378526
SSAFY <SSAFY@DESKTOP-DOGVPUB>
2023-02-08 11:37:22 +0900

    수열 부분합

--- algorithm/src/backjoon/ex11659/Main.java
+++ algorithm/src/backjoon/ex11659/Main.java
@@ -1,19 +1,25 @@
 package backjoon.ex11659;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
 	static int[] arr;
+	static int[] sumarr;
 	static int sum;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	private static void sum(int start, int end) {
-		sum = 0;
-		for(int i=start-1; i<=end-1; i++) {
-			sum += arr[i];
-		}
-		System.out.println(sum);
+	private static void sum(int start, int end) throws IOException {
+		if(start-2>=0)
+			sum = sumarr[end-1]-sumarr[start-2];
+		else
+			sum = sumarr[end-1];
+		
+		bw.append(sum+"\n");
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -24,15 +30,22 @@ public class Main {
 		int N = Integer.parseInt(str[0]);
 		int M = Integer.parseInt(str[1]);
 		arr = new int[N];
+		sumarr = new int[N];
 		
 		str = br.readLine().split(" ");
-		for(int i=0; i<N; i++) {
+		sumarr[0] = Integer.parseInt(str[0]);
+		arr[0] = Integer.parseInt(str[0]);
+		for(int i=1; i<N; i++) {
 			arr[i] = Integer.parseInt(str[i]);
+			sumarr[i] = sumarr[i-1]+arr[i];
 		}
 		
 		for(int i=0; i<M; i++) {
 			str = br.readLine().split(" ");
 			sum(Integer.parseInt(str[0]), Integer.parseInt(str[1]));
 		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
