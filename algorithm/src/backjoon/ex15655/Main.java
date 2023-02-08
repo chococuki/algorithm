ad853f8a0670320350c73621346e8ef83280da10
SSAFY <SSAFY@DESKTOP-DOGVPUB>
2023-02-08 10:43:45 +0900

    재귀 중복x 등차수열

---
+++ algorithm/src/backjoon/ex15655/Main.java
@@ -0,0 +1,49 @@
+package backjoon.ex15655;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+	static int N, M;
+	static int[] numbers;
+	static List<Integer> list = new ArrayList<>();
+	
+	private static void find(int cnt, int start) {
+		if(cnt==M) {
+			for(int i=0; i<M; i++) {
+				System.out.print(numbers[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=start; i<N; i++) {
+			numbers[cnt] = list.get(i);
+			find(cnt+1, i+1);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str;
+		str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+		
+		numbers = new int[M];
+		
+		str = br.readLine().split(" ");
+		for(int i=0; i<N; i++) {
+			list.add(Integer.parseInt(str[i]));
+		}
+		
+		Collections.sort(list);
+		
+		find(0, 0);
+	}
+}
